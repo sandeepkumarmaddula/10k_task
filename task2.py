098c413849ef6bd2497b697cc13b71c5a327e0de
sandeepkumarmaddula <sandeepmaddula31@gmail.com>
2026-07-07 10:46:47 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,31 @@
+#1. XOR Operation
+a = 5
+b = 5
+print(a ^ b)
+#2. AND Operation
+a = 5
+b = 3
+print(a & b)
+#3. Is 1 Even or Odd
+num = 1
+if num & 1:
+    print("Odd")
+else:
+    print("Even")
+#All Bitwise Operators
+a = 5    # 0101
+b = 3    # 0011
+print("a =", a)
+print("b =", b)
+# AND
+print("a & b =", a & b)
+# OR
+print("a | b =", a | b)
+# XOR
+print("a ^ b =", a ^ b)
+# NOT
+print("~a =", ~a)
+# Left Shift
+print("a << 1 =", a << 1)
+# Right Shift
+print("a >> 1 =", a >> 1)
