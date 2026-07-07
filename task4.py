6793dd34e690339b017512b81b3fba85625755de
sandeepkumarmaddula <sandeepmaddula31@gmail.com>
2026-07-07 10:59:15 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,21 @@
+#1. Missing quotes in input()
+name = input(Enter your name:)
+#2. Missing parentheses in print()
+print "Hello"
+#3. Adding string and integer
+age = input("Enter age: ")
+print("Age: " + age + 5)
+#4. Using input() without type conversion
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+print(a + b)
+#5. Misspelled print
+pritn("Hello")
+#6. Missing closing quotation mark
+print("Python)
+#7. Missing closing parenthesis
+print("Welcome"
+#8. Invalid integer input
+age = int(input("Enter age: "))
+#9. Using an undefined variable
+print(name)
